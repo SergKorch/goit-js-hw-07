7bab63044aa4cb903f736a0aced5d1116e1875c2
Serhii Korchemkin <skorchemkin1987@gmail.com>
2022-02-06 21:10:49 +0200

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,10 +1,14 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.table(galleryItems);
-const listEl = galleryItems.map((item)=>{
-    return `<img src="${item.original}">`}).join('')
-console.log(listEl)
-const listImg = document.querySelector('div[class="gallery"]')
-console.log(listImg)
-listImg.innerHTML = listEl
+const listEl = galleryItems
+  .map((item) => {
+    return `<img src="${item.original}">`;
+  })
+  .join("");
+console.log(listEl);
+const listImg = document.querySelector('div[class="gallery"]');
+console.log(listImg);
+listImg.innerHTML = listEl;
+listEl.classList.add("gallery__image");
