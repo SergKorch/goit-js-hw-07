46a0449951e291eb5973302472c42043b9bbf9be
Serhii Korchemkin <skorchemkin1987@gmail.com>
2022-02-07 20:02:32 +0200

    +

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -25,59 +25,28 @@ galleryElements.insertAdjacentHTML("beforeend", makeGalleryMarkup);
 galleryElements.addEventListener("click", onGalleryClick);
 
 function onGalleryClick(event) {
-  console.log(event.target.src);
   if (event.target.nodeName !== "IMG") {
     return;
   }
   event.preventDefault();
-  const instance = basicLightbox.create(`
-    
-    <div class="modal">
-        // <img src="${event.target.dataset.source}" style="height:50vh; display:block"></img>
-    </div>
-`);
-  console.log(event.target.dataset.source);
-  instance.show();
+  modalImg(event.target.dataset.source);
 }
 
-// function onGalleryClick(event) {
-//   if (event.target.nodeName !== "IMG") {
-//     return;
-//   }
-//   event.preventDefault();
-//   modalShow(event.target.dataset.source);
-// }
-// let instance;
+function modalImg(source) {
+  const instance = basicLightbox.create(`
+    
+  <div class="modal">
+       <img src="${source}" style="height:50vh; display:block"></img>
+  </div>
+`);
+  instance.show();
+  escapeOut(instance);
+}
 
-// function modalShow(src) {
-//   instance = basicLightbox.create(
-//     `
-//     <div class="modal">
-//         <img src="${src}" style="height:50vh; display:block"></img>
-//     </div>
-// `,
-//     {
-//       onShow: (instance) => {
-//         addListener();
-//       },
-//       onClose: (instance) => {
-//         removeListener();
-//       },
-//     }
-//   );
-//   instance.show();
-// }
-
-// function addListener() {
-//   window.addEventListener("keydown", onEscClick);
-// }
-
-// function onEscClick(event) {
-//   if (event.code === "Escape") {
-//     instance.close();
-//   }
-// }
-
-// function removeListener() {
-//   window.removeEventListener("keydown", onEscClick);
-// }
+function escapeOut(escape) {
+  document.addEventListener("keydown", (event) => {
+    if (event.key) {
+      escape.close();
+    }
+  });
+}
