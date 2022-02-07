23c3b0c563a73c8d2ec96e32953f3cb83f113651
Serhii Korchemkin <skorchemkin1987@gmail.com>
2022-02-07 17:11:01 +0200

    +

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,14 +1,67 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.table(galleryItems);
-const listEl = galleryItems
-  .map((item) => {
-    return `<img src="${item.original}">`;
-  })
-  .join("");
-console.log(listEl);
-const listImg = document.querySelector('div[class="gallery"]');
-console.log(listImg);
-listImg.innerHTML = listEl;
-listEl.classList.add("gallery__image");
+const makeGalleryItemMarkup = ({ preview, original, description }) => {
+  return `<div class="gallery__item">
+    <a class="gallery__link" href="${original} target="_self">
+      <img
+        class="gallery__image"
+        style="display:block"
+        data-lightbox="roadtrip"
+        src="${preview}"
+        data-source="${original}"
+        alt="Image ${description}"
+      />
+    </a>
+  </div>
+    `;
+};
+const makeGalleryMarkup = galleryItems.map(makeGalleryItemMarkup).join('');
+
+const galleryElements = document.querySelector('.gallery');
+
+galleryElements.insertAdjacentHTML('beforeend', makeGalleryMarkup);
+
+galleryElements.addEventListener('click', onGalleryClick);
+
+function onGalleryClick(event) {
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+  event.preventDefault();
+  modalShow(event.target.dataset.source);
+}
+let instance;
+
+function modalShow(src) {
+  instance = basicLightbox.create(
+    `
+    <div class="modal">
+        <img src="${src}" style="height:50vh; display:block"></img>
+    </div>
+`,
+    {
+      onShow: (instance) => {
+        addListener();
+      },
+      onClose: (instance) => {
+        removeListener();
+      },
+    }
+  );
+  instance.show();
+}
+
+function addListener() {
+  window.addEventListener("keydown", onEscClick);
+}
+
+function onEscClick(event) {
+  if (event.code === "Escape") {
+    instance.close();
+  }
+}
+
+function removeListener() {
+  window.removeEventListener("keydown", onEscClick);
+}
